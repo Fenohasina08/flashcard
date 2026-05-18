bb3b3b9c4c81153e5d6bc80d4b3a529faa130a04
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-18 19:13:04 +0300

    refactor(section): refactor the percentage of the parties

--- flashcard/app/page.tsx
+++ flashcard/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="flex flex-col w-full h-screen">
-    <div className="flex w-full h-[10%] bg-red-500 text-black">Flash Card</div>
-    <div className="flex w-full h-[10%] bg-yellow-400">Loading</div>
-    <div className="flex w-full h-[80%] bg-amber-950">Content</div>
+  <div className=" w-full h-screen bg-white flex flex-col justify-center items-center">
+      <div className="flex w-[80%] h-[5%] bg-red-500 text-black">Flash Card</div>
+      <div className="flex w-[80%] h-[10%] bg-yellow-400">Loading</div>
+      <div className="flex w-[80%] h-[85%] bg-amber-950">Content</div> 
   </div>
   );
 }
