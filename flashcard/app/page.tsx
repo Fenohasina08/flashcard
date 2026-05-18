2830331fd0f205624053cc57244eb2581d1bcdbc
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-18 21:43:18 +0300

    refactor(app-page.tsx): refactor to components

--- flashcard/app/page.tsx
+++ flashcard/app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
+import Loading from "@/components/Loading";
+import Flashcard from "@/components/Flashcard";
+import Content from "@/components/Content";
 
 export default function Home() {
   return (
   <div className="w-full h-screen flex items-center justify-center background-primary text-black">
     <div className="flex flex-col w-[80%] h-[80%]">
-      <div className="h-[5%] bg-red-500">Flash Card</div>
-      <div className="h-[10%] bg-yellow-400">Loading</div>
-      <div className="h-[85%] bg-amber-950">Content</div>
+      <div className="h-[5%] bg-red-500">
+        <Flashcard />
+      </div>
+      <div className="h-[10%] bg-yellow-400">
+        <Loading />
+      </div>
+      <div className="h-[85%] bg-amber-950">
+        <Content />
+      </div>
     </div>
   </div>
   );
