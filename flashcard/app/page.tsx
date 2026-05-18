420da1b350b628ce094b12655af7759112093605
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-18 21:13:18 +0300

    feat(global.css): comments the dark mode for now

--- flashcard/app/page.tsx
+++ flashcard/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<div className="w-full h-screen flex items-center justify-center background-primary">
-  <div className="flex flex-col w-[80%] h-[80%]">
-    <div className="h-[5%] bg-red-500">Flash Card</div>
-    <div className="h-[10%] bg-yellow-400">Loading</div>
-    <div className="h-[85%] bg-amber-950">Content</div>
+  <div className="w-full h-screen flex items-center justify-center background-primary text-black">
+    <div className="flex flex-col w-[80%] h-[80%]">
+      <div className="h-[5%] bg-red-500">Flash Card</div>
+      <div className="h-[10%] bg-yellow-400">Loading</div>
+      <div className="h-[85%] bg-amber-950">Content</div>
+    </div>
   </div>
-</div>
   );
 }
